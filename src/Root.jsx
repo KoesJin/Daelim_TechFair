e7649325a1612992d4a63d01bfe23a771153ee87
KIMHYEONSU <piae4728@gmail.com>
2024-10-19 00:46:53 +0900

    make mainpage

--- src/Root.jsx
+++ src/Root.jsx
@@ -16,7 +16,7 @@ const GlobalStyle = createGlobalStyle`
     }
 
     body {
-      overflow-y: hidden; /* 가로 스크롤을 막되 세로 스크롤은 허용 */
+      overflow-y: scroll; /* 가로 스크롤을 막되 세로 스크롤은 허용 */
     }
 
     .fade-enter {
--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -3,15 +3,27 @@ import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import BalanceBox from '../components/BalanceBox';
 import styles from '../css/MainPage.module.css';
+import { FaSearch } from 'react-icons/fa';
 
 const MainPage = () => {
   const navigate = useNavigate();
-
   const balanceBoxes = new Array(12).fill(0);
 
   return (
     <>
       <Header />
+      <div className={styles.splitContainer}>
+        <div className={styles.leftSection}>
+          <h1>밸런스게임</h1>
+          <div className={styles.searchContainer}>
+            <FaSearch className={styles.searchIcon} />
+            <input type="text" placeholder="검색어를 입력하세요" className={styles.searchBar} />
+          </div>
+        </div>
+        <div className={styles.rightSection}>
+          <div className={styles.chatBox}>실시간 채팅</div>
+        </div>
+      </div>
       <div className={styles.gridContainer}>
         {balanceBoxes.map((_, index) => (
           <BalanceBox key={index} />
