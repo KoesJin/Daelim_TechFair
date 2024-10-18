6e99269a3005a3ec53952797038fc12c0571af4e
KIMHYEONSU <piae4728@gmail.com>
2024-10-18 21:28:15 +0900

    Modify header button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styles from '../css/Header.module.css';
 
 const Header = () => {
+  const navigate = useNavigate();
+  
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
-        {/* 로고 클릭 시 메인 페이지로 이동 */}
-        <Link to="/" className={styles.logo}>
-          밸런스게임
-        </Link>
+        <div 
+          className={styles.logo}
+          onClick={() => navigate('/')}
+        >밸런스게임
+        </div>
       </div>
       <nav className={styles.nav}>
-        {/* 카테고리 버튼 */}
-        <Link to="/balance" className={styles.navItem}>
-          밸런스 게임
-        </Link>
-        <Link to="/vote" className={styles.navItem}>
-          찬반 투표
-        </Link>
+        <div 
+          className={styles.navItem}
+          onClick={() => navigate('/balance')}
+        >밸런스 게임
+        </div>
+        <div 
+          className={styles.navItem}
+          onClick={() => navigate('/vote')}
+        >찬반 투표
+        </div>
       </nav>
       <div className={styles.authButtons}>
-        {/* 로그인 및 회원가입 버튼 */}
-        <Link to="/signin" className={styles.authButton}>
-          로그인
-        </Link>
-        <Link to="/signup" className={styles.authButton}>
-          회원가입
-        </Link>
+        <div 
+          className={styles.authButton}
+          onClick={() => navigate('/signinpage')}
+        >로그인
+        </div>
+        <div 
+          className={styles.authButton}
+          onClick={() => navigate('/signuppage')}
+        >회원가입
+        </div>
       </div>
     </header>
   );
