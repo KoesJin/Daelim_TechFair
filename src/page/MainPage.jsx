9eafceb2c15c2330edd4f80be877a92872813032
KIMHYEONSU <piae4728@gmail.com>
2024-10-18 21:49:14 +0900

    Add Boxs

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import BalanceBox from '../components/BalanceBox';
-import styles from '../css/MainPage.module.css';  // CSS 모듈을 따로 만들어서 스타일 적용
+import styles from '../css/MainPage.module.css';
 
 const MainPage = () => {
   const navigate = useNavigate();
 
-  // 배열을 만들어 BalanceBox 여러 개 렌더링하기
-  const balanceBoxes = new Array(12).fill(0);  // 예시로 12개를 렌더링
+  const balanceBoxes = new Array(12).fill(0);
 
   return (
     <>
